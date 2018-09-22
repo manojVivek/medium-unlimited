a560beabfced5dc125c99442a76d76224d52ee16
Manoj Vivek <p.manoj.vivek@gmail.com>
2018-09-22 10:01:30 +0530

    Bug fix

--- src/analytics.js
+++ src/analytics.js
@@ -10,17 +10,23 @@ function _getAmplitudeClient() {
     return Promise.resolve(client);
   }
   return new Promise((resolve, reject) => {
+    if (getUserId()) {
+      client = _createClient(getUserId());
+      return resolve(client);
+    }
     chrome.runtime.sendMessage({type: FETCH_USER_ID}, response => {
       log('Received response for userId', response);
       if (response.status != 'SUCCESS') {
         return reject(response);
       }
-      client = new AmplitudeClient(amplitudeApiKey(), {
-        user_id: response.userId,
-      });
+      client = _createClient(response.userId);
       return resolve(client);
     });
   });
+}
+
+function _createClient(user_id) {
+  return new AmplitudeClient(amplitudeApiKey(), {user_id});
 }
 
 export function track(event_type) {
