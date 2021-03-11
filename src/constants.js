b9a9d2164a18192aab1b78d4ff970b5a2d8f67d1
Manoj Vivek <p.manoj.vivek@gmail.com>
2021-03-11 09:12:52 +0530

    Added a fix to reload the page to unlock the article with twitter header

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const CONTENT_SECTION_CLASSNAME = 'section-content';
 export const MEMBERSHIP_PROMPT_CLASSNAME = 'postFade';
+export const MEMBERSHIP_PROMPT_ID = 'paywall-background-color';
 
 export const USER_ID_KEY = 'userId';
 export const READ_COUNT_KEY = 'readCount';
